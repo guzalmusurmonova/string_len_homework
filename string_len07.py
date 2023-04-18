67c5a82577a4c2c4f30c25b3b4b6fc2b9afe2f02
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-18 01:48:53 -0700

    07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,19 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    if len(s1)%2!=0:
+        s1
+    else:
+        s1=""
+    if len(s2)%2!=0:
+        s2 
+    else:
+        s2="" 
+    if len(s3)%2!=0:
+        s3 
+    else: 
+        s3=""
+
+    
+    return '"['+s1+s2+s3+']"' 
+print(main('code','coder','python'))
